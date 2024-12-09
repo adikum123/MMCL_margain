ecb82a8ff8da7391d8ce089db8d6a7475400db3b
Admir <admir@qyobo.com>
2024-12-09 20:01:00 +0100

    Small update

--- svm_solver.py
+++ svm_solver.py
@@ -63,7 +63,9 @@ class SVMSolver:
             result[params_key] = defaultdict(list)
             for test_point in self.test_loader:
                 X = np.vstack((test_point[0], train_data))
-                Y = np.vstack((np.ones(1), -np.ones(len(self.train_loader.dataset))))
+                Y = np.vstack(
+                    (np.ones(1, 1), -np.ones(len(self.train_loader.dataset, 1)))
+                )
                 # Train SVM using the stored parameters
                 model = SVC(**params)
                 model.fit(X, Y)
